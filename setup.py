cdd648cb5ddc8f6fd528c1b7be0c65a123043042
lsiden <lawrence.siden@verizonwireless.com>
2019-10-17 19:03:47 -0400

    Refactors project for packaging

--- setup.py
+++ setup.py
@@ -17,8 +17,9 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=['requests']
 )
