093616a92482811dc74e0e900bb0f3a152c5a6bc
Lawrence Siden <lsiden@gmail.com>
2019-11-18 12:26:46 -0500

    Update version to 0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cert_session",
-    version="0.0.4",
+    version="0.0.3",
     author="Lawrence Siden",
     author_email="lsiden@gmail.com",
     description="'requests' session that appends custom certificate authority",
